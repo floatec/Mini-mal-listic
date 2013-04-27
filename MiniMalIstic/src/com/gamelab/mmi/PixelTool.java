6b7ff46a97cea9444a832ec243b86ee373e32eac
blah <omg@omg.com>
2013-04-27 20:08:09 +0200

    zth

--- MiniMalIstic/src/com/gamelab/mmi/PixelTool.java
+++ MiniMalIstic/src/com/gamelab/mmi/PixelTool.java
@@ -1,18 +1,28 @@
 package com.gamelab.mmi;
 
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.TextureData;
-import com.badlogic.gdx.graphics.g2d.PixmapPacker;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 public class PixelTool extends Tool {
 
-	@Override
-	public void draw(Vector2 start, Vector2 end, float distance) {
-		// TODO Auto-generated method stub
-		FrameBuffer buf = new FrameBuffer(null, 0, 0, false);
+	public PixelTool(Pixmap pixmap, Sprite sprite, Texture texture) {
+		super(pixmap, sprite, texture);
 	}
+	
+	@Override
+	public void draw(Vector2 curPos, Vector2 lastPos, float radius,
+			float distance) {
+		pixmapHelper.pixmap.drawPixel(0, 0, 0xff888888);
+		pixmapHelper.pixmap.drawPixel(0, 1, 0xff888888);
+		pixmapHelper.pixmap.drawPixel(0, 2, 0xff888888);
+		pixmapHelper.pixmap.drawPixel(0, 3, 0xff888888);
+		pixmapHelper.pixmap.drawPixel(0, 4, 0xff888888);
+		pixmapHelper.pixmap.drawPixel(0, 5, 0xff888888);
+		pixmapHelper.pixmap.drawPixel(0, 6, 0xff888888);
+		pixmapHelper.update();
+	}
+
 
 }
--- MiniMalIstic/src/com/gamelab/mmi/Tool.java
+++ MiniMalIstic/src/com/gamelab/mmi/Tool.java
@@ -1,11 +1,18 @@
 package com.gamelab.mmi;
 
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 public abstract class Tool {
 	
+	protected PixmapHelper pixmapHelper;
 	
-
-	public abstract void draw(Vector2 start, Vector2 end, float distance);
+	public Tool(Pixmap pixmap, Sprite sprite, Texture texture) {
+		pixmapHelper = new PixmapHelper(pixmap, sprite, texture);
+	}
+	
+	public abstract void draw(Vector2 curPos, Vector2 lastPos, float radius, float distance);
 	
 }

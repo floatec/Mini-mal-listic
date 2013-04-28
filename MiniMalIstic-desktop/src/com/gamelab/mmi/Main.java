fadf37b1bc910b1c118002889236849656711eb9
Justus Ulbrich <justus.ulbrich@student.kit.edu>
2013-04-28 14:11:54 +0200

    added pics

--- MiniMalIstic-desktop/src/com/gamelab/mmi/Main.java
+++ MiniMalIstic-desktop/src/com/gamelab/mmi/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "MiniMalIstic";
 		cfg.useGL20 = true;
-		cfg.width = 512;
-		cfg.height = 512;
+		cfg.width = 1024;
+		cfg.height = 768;
 		
 		new LwjglApplication(new Mmi(), cfg);
 	}
